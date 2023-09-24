d9df814fa13bc2725abd2e0f8486901fc8731862
sallai-xu <145926353+sallai-xu@users.noreply.github.com>
2023-09-25 01:20:12 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -124,6 +124,7 @@ module.exports = {
           },
         },
       });
+    
       // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
       config.optimization.runtimeChunk("single");
     });
