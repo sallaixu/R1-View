ffd118df33b7d54b0837c5745b1185966747ab3c
sallai <48641847+sallai1@users.noreply.github.com>
2023-09-25 12:01:30 +0000

    添加滚动歌词

---
+++ src/components/music/lyric-view.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="lyrics-container">
+    <div class="lyric-line" v-for="(line, index) in lyricLines" :key="index" :class="{ active: index === currentLine }">
+      {{ line.txt }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Lyric from "lyric-parser";
+export default {
+  name: "lyricView",
+  data() {
+    return {
+      lyric: null,
+      lyricLines: [],
+      currentLine: 0,
+    };
+  },
+  methods: {
+    setLyric(lyric) {
+      this.lyric = new Lyric(lyric, this.handleLyric);
+    },
+    handleLyric({ lineNum }) {
+      this.currentLine = lineNum;
+      this.lyricLines = this.lyric.lines.slice(Math.max(lineNum - 5, 0), lineNum + 6);
+    },
+    handleMusicTimeUpdate(currentTime) {
+      // 更新当前播放时间
+      this.currentTime = currentTime;
+      // 歌词解析器根据当前播放时间进行滚动显示
+      this.lyric.seek(currentTime * 1000);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.lyrics-container {
+  height: 300px; /* 歌词容器的高度 */
+  overflow: hidden;
+  position: relative;
+  line-height: 24px; /* 歌词行高度 */
+}
+
+.lyric-line {
+  text-align: center;
+  transition: 0.3s ease-in-out; /* 添加过渡效果 */
+}
+
+.lyric-line.active {
+  font-weight: bold; /* 激活状态的歌词加粗显示 */
+}
+</style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en"; // lang i18n
 
-import '@/styles/index.scss' // global css
+import "@/styles/index.scss"; // global css
 
-import App from './App'
-import store from './store'
-import router from './router'
+import App from "./App";
+import store from "./store";
+import router from "./router";
 
-import '@/icons' // icon
-import '@/permission' // permission control
+import "@/icons"; // icon
+import "@/permission"; // permission control
 
 // 按需引入 引入 ECharts 主模块
-import echarts from 'echarts'
+import echarts from "echarts";
 //将echarts添加到vue的原型上
-Vue.prototype.$echarts = echarts
+Vue.prototype.$echarts = echarts;
 
 /**
  * If you don't want to use mock-server
@@ -28,27 +28,29 @@ Vue.prototype.$echarts = echarts
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
+if (process.env.NODE_ENV === "production") {
+  const { mockXHR } = require("../mock");
+  mockXHR();
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale });
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // 导入GlobalLoading组件
-import GlobalLoading from './components/loading/loading-1';
+import GlobalLoading from "./components/loading/loading-1";
+import LyricView from "./components/music/lyric-view";
 
 // 全局注册GlobalLoading组件
-Vue.component('GlobalLoading', GlobalLoading);
+Vue.component("GlobalLoading", GlobalLoading);
+Vue.component("LyricView", LyricView);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});
